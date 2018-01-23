6a79868e2823b872928caefcfc51f761b72fb2ca
ELEXILON <elexilon@gmail.com>
2018-01-23 10:25:37 +0100

    create routes

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+import { SignIn, SignUp } from './containers'
+
+export default class Routes extends Component {
+  render() {
+    return (
+      <div>
+        <Route exact path="/"  />
+        <Route path="/sign-in" component={SignIn} />
+        <Route path="/sign-up" component={SignUp} />
+      </div>
+    )
+  }
+}
